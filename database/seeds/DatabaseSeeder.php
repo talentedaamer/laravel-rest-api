ee0701cc85537e1d1cee1e18256666aa51682a56
Aamer Shahzad <a.shahzad@airtango.pk>
2019-05-10 00:32:45 +0500

    complete seeding part

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        // factory(\App\Models\User::class, 10)->create();
+        factory(\App\Models\User::class, 10)->create();
         factory(\App\Models\Post::class, 50)->create();
         factory(\App\Models\Comment::class, 300)->create();
     }
